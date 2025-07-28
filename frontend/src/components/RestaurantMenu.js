21e6385fb9139eadb7d2c20d4a45807d07820675
Rishabh Damle <dwrushabh@gmail.com>
2025-07-28 19:58:18 +0530

    changes has been made on 28/07/2025 - 07:58 PM

--- frontend/src/components/RestaurantMenu.js
+++ frontend/src/components/RestaurantMenu.js
@@ -1,32 +1,39 @@
 import { useEffect, useState } from "react";
 import { FETCH_MENU_URL } from "../utils/config";
 import { useParams } from "react-router";
+import ShimmerUI from "./ShimmerUI";
 function RestaurantMenu() {
-  const [restaurantData, setRestaurantData] = useState();
+  const [restaurantData, setRestaurantData] = useState(null);
 
   useEffect(() => {
-    fetchData();
+    fetchMenu();
   }, []);
 
   const { resId } = useParams();
   console.log("ResId : " + resId);
-  async function fetchData() {
+  async function fetchMenu() {
     const data = await fetch(FETCH_MENU_URL + resId);
     const json = await data.json();
     console.log(json);
     setRestaurantData(json);
   }
+  if (restaurantData == null) {
+    return <ShimmerUI></ShimmerUI>;
+  }
+
+  const { name, city, costForTwoMessage, cuisines, avgRating } =
+    restaurantData?.data?.cards[2]?.card?.card?.info;
 
   return (
     <>
-      <h1>{restaurantData?.data?.cards[2]?.card?.card?.info?.name}</h1>
-      <h2>{restaurantData?.data?.cards[2]?.card?.card?.info?.city}</h2>
-      <h3>
-        {restaurantData?.data?.cards[2]?.card?.card?.info?.costForTwoMessage}
-      </h3>
-      <h3>
-        {restaurantData?.data?.cards[2]?.card?.card?.info?.cuisines.join(", ")}
-      </h3>
+      <div>
+        {" "}
+        <h1>Restaurant Name - {name}</h1>
+        <h2>City - {city}</h2>
+        <h3>Price - {costForTwoMessage}</h3>
+        <h3>Cuisines -{cuisines.join(", ")}</h3>
+        <h3>Ratings - {avgRating}</h3>
+      </div>
     </>
   );
 }
