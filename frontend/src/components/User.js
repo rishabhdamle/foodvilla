2e78f70e542b77a3ab3dceb24fd2fb184d7514e0
Rishabh Damle <dwrushabh@gmail.com>
2025-07-30 15:48:23 +0530

    changes has been made on 3:50PM - 30/07/2025

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -2,14 +2,29 @@ import React from "react";
 class User extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      salaryPackage: "1cr",
+    };
   }
 
   render() {
     const { name, designation } = this.props;
+    const { count, salaryPackage } = this.state;
     return (
       <div>
-        <h1>{name}</h1>
-        <h2>{designation}</h2>
+        <h1>Name : {name}</h1>
+        <h2>Designation : {designation}</h2>
+        <h3>No of years in the company : {count}</h3>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increase
+        </button>
+        <h3>Total package : {salaryPackage}</h3>
       </div>
     );
   }
