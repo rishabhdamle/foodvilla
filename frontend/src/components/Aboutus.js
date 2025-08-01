31a0749d2f862387d470e7f751167ff034a281fa
Rishabh Damle <dwrushabh@gmail.com>
2025-08-01 17:57:03 +0530

    5:56 PM - 1/8/2025

--- frontend/src/components/Aboutus.js
+++ frontend/src/components/Aboutus.js
@@ -1,10 +1,21 @@
 import User from "./User";
-function Aboutus() {
-  return (
-    <div>
-      <User name={"Rishabh"} designation={"Co-Founder & CEO"}></User>
-    </div>
-  );
+import React from "react";
+class Aboutus extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent constructor is called");
+  }
+  componentDidMount() {
+    console.log("Parent component is mounted");
+  }
+  render() {
+    console.log("Parent render is called");
+    return (
+      <div>
+        <User name={"Rishabh"} designation={"Co-Founder & CEO"}></User>
+      </div>
+    );
+  }
 }
 
 export default Aboutus;
--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -7,11 +7,15 @@ class User extends React.Component {
       count: 0,
       salaryPackage: "1cr",
     };
+    console.log("Child constructor is called");
   }
-
+  componentDidMount() {
+    console.log("Child component is mounted");
+  }
   render() {
     const { name, designation } = this.props;
     const { count, salaryPackage } = this.state;
+    console.log("Child render is called");
     return (
       <div>
         <h1>Name : {name}</h1>
@@ -19,12 +23,23 @@ class User extends React.Component {
         <h3>No of years in the company : {count}</h3>
         <button
           onClick={() => {
-            this.setState({ count: this.state.count + 1 });
+            this.setState({
+              count: this.state.count + 1,
+            });
           }}
         >
           Increase
         </button>
         <h3>Total package : {salaryPackage}</h3>
+        <button
+          onClick={() => {
+            this.setState({
+              salaryPackage: (this.state.salaryPackage = "2cr"),
+            });
+          }}
+        >
+          Increase
+        </button>
       </div>
     );
   }
